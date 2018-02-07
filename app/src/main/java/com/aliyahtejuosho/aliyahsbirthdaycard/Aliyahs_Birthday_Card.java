fd104cfde6cdedd98cf06f3c6713599199d1346e
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-02-07 09:42:44 -0600

    Initial commit

---
+++ app/src/main/java/com/aliyahtejuosho/aliyahsbirthdaycard/Aliyahs_Birthday_Card.java
@@ -0,0 +1,13 @@
+package com.aliyahtejuosho.aliyahsbirthdaycard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Aliyahs_Birthday_Card extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aliyahs__birthday__card);
+    }
+}
